f6144c4c25692e062973aaa8d0dcd98cdc60d1d6
Dragos313 <pana749@gmail.com>
2024-01-17 16:20:43 +0200

    Second commit

--- InregistrareStudenti/Models/StudentRegistration.cs
+++ InregistrareStudenti/Models/StudentRegistration.cs
@@ -3,15 +3,8 @@
     public class StudentRegistration
     {
         public long StudentId { get; set; }
-        public string FullName { get; set; }
-
         public long UserId { get; set; }
-        public string Email { get; set; }
-        public string Specialization { get; set; }
-
         public long SpecId { get; set; }
-        public string Tax { get; set; }
-        public string Years { get; set; }
         public string RegisterDate { get; set; }
     }
 }
