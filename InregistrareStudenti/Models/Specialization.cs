15c4e4e00a9f4a53235f1b4d8cbdc4f3bfc8f17c
Dragos313 <pana749@gmail.com>
2024-01-17 15:26:01 +0200

    first commit

---
+++ InregistrareStudenti/Models/Specialization.cs
@@ -0,0 +1,12 @@
+﻿namespace InregistrareStudenti.Models
+{
+    public class Specialization
+    {
+        public long SpecId { get; set; }
+        public string Name { get; set; }
+
+        public string Tax { get; set; }
+
+        public string Years { get; set; }
+    }
+}
---
+++ InregistrareStudenti/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace InregistrareStudenti.Models
+{
+    public class User
+    {
+        public long UserId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
